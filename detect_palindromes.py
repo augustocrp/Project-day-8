7674aed821ded13dc52a8565c16d36935ca8cd78
Augusto Cesar <dev.augusto.crp@gmail.com>
2022-12-24 15:22:13 -0300

    Update detect_palindromes.py

--- detect_palindromes.py
+++ detect_palindromes.py
@@ -15,6 +15,12 @@ def is_palindrome(word):
     else:
         return False
 
+def is_palindrome_recursive(word):
+    if len(word) <= 1:
+        return True
+    else:
+        return word[0] == word[-1] and is_palindrome_recursive(word[1:-1])
+
 words = ["arara", "racecar", "carro", "cama", "level"]
 
 for word in words:
