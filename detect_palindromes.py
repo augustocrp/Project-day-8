92f64e566c84c6b8b30abcd34c8f48d66924cb1b
Augusto Cesar <dev.augusto.crp@gmail.com>
2022-12-24 15:14:03 -0300

    Create detect_palindromes.py

---
+++ detect_palindromes.py
@@ -0,0 +1,12 @@
+import math
+
+def is_palindrome(word):
+    j = len(word)-1
+    result = 0
+    for i in range(len(word)):
+        if word[i] == word[j]:
+            result = result +1
+        if i >= j:
+            break
+        j = j - 1
+
